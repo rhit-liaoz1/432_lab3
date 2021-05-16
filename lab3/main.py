9e0508125b9968bafa72cb744fce77552d8a74e6
L7907661 <hlbtaioe@163.com>
2021-05-16 11:45:10 -0400

    changes again

--- lab3/main.py
+++ lab3/main.py
@@ -92,36 +92,36 @@ def main():
 
 def thread_helper(conn, address, se):
     # print(conn)
-    # rec = conn.recv(2024)
-    # print(rec)
+    rec = conn.recv(2024)
+    print(rec)
     # # html = getURL(url)
-    # url = rec.split('\n')[0].split(' ')[1]
-    # print("url:",url)
-    # http  = url.find("://")
-    # if (http==-1):
-    #     temp = url
-    # else:
-    #     temp = url[(http+3):]
-    # pp = temp.find(":")
-    # # find web server
-    # webserver_pos = temp.find("/")
-    # if webserver_pos == -1:
-    #     webserver_pos = len(temp)
-    # webserver = ""
-    # port = -1
-    # if (pp == -1 or webserver_pos < pp):
-    #     port = 4000
-    #     webserver = temp[:webserver_pos]
-    # else:
-    #     port = int((temp[(pp + 1):])[:webserver_pos - pp - 1])
-    #     webserver = temp[:pp]
+    url = rec.split('\n')[0].split(' ')[1]
+    print("url:",url)
+    http  = url.find("://")
+    if (http==-1):
+        temp = url
+    else:
+        temp = url[(http+3):]
+    pp = temp.find(":")
+    # find web server
+    webserver_pos = temp.find("/")
+    if webserver_pos == -1:
+        webserver_pos = len(temp)
+    webserver = ""
+    port = -1
+    if (pp == -1 or webserver_pos < pp):
+        port = 4000
+        webserver = temp[:webserver_pos]
+    else:
+        port = int((temp[(pp + 1):])[:webserver_pos - pp - 1])
+        webserver = temp[:pp]
 
     # connect to webserver
     try:
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         # bind
-        server_socket.connect((Host, port))
+        server_socket.connect((webserver, port))
 
         # listen
         server_socket.send(se)
